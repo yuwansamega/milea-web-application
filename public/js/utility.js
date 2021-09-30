7b4a7c6068276e72703459ca03b018d99a9b3202
Muhammad Sholeh <sholehbaabud11@gmail.com>
2021-09-30 09:14:03 +0700

    view user
    +beranda
    +daftar kegiatan
    +detail-profil
    +detail-kegiatan
    + lengkapi profil

---
+++ public/js/utility.js
@@ -0,0 +1,47 @@
+const modal = document.getElementById("modal");
+const modalButton = document.getElementsByClassName("modal-button");
+
+const navDropdown = document.getElementById("dropdown");
+const navDropdownToogle = document.getElementsByClassName("dropdown-toggle");
+
+const navMobileDropdown = document.getElementById("mobile-dropdown");
+const navMobileDropdownToogle = document.getElementsByClassName(
+  "mobile-dropdown-toggle"
+);
+
+const addInteractions = function (trigger, target) {
+  console.log(trigger);
+  console.log(target);
+
+  for (let index = 0; index < trigger.length; index++) {
+    trigger[index].addEventListener("click", () => {
+      const targetDisplay = target.style.display;
+
+      if (targetDisplay == "" || targetDisplay == "none")
+        target.style.display = "flex";
+      else target.style.display = "none";
+    });
+  }
+};
+
+addInteractions(modalButton, modal);
+addInteractions(navDropdownToogle, navDropdown);
+addInteractions(navMobileDropdownToogle, navMobileDropdown);
+
+window.addEventListener("scroll", (e) => {
+  const nav = document.getElementsByTagName("nav")[0];
+  const whiteToggle = document.getElementById("toogle-white");
+  const blackToggle = document.getElementById("toogle-black");
+
+  if (window.scrollY >= 97) {
+    nav.style.backgroundColor = "white";
+    blackToggle.style.display = "block";
+    whiteToggle.style.display = "none";
+    nav.style.boxShadow = "0px 5px 14px rgba(0, 0, 0, 0.12)";
+  } else {
+    nav.style.backgroundColor = "unset";
+    blackToggle.style.display = "none";
+    whiteToggle.style.display = "block";
+    nav.style.boxShadow = "none";
+  }
+});
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/beranda', function () {
+    return view('beranda');
+});
+Route::get('/data-profil', function () {
+    return view('data-profil');
+});
+Route::get('/lengkapi-profil', function () {
+    return view('lengkapi-profil');
+});
+Route::get('/daftar-kegiatan', function () {
+    return view('daftar-kegiatan');
+});
+Route::get('/detail-kegiatan', function () {
+    return view('detail-kegiatan');
+});Route::get('/riwayat', function () {
+    return view('riwayat');
+});
